fa2c7e42e8bc574876b602c0645ad345ec5360be
Budi Sulistiyo <budimantaro@gmail.com>
2024-09-18 17:30:01 +0700

    Update halaman_input.php

--- admin/halaman_input.php
+++ admin/halaman_input.php
@@ -4,18 +4,61 @@ include("inc_header.php");
 
 <!-- kita buat variabel kosong dahulu -->
 <?php
-    $judul      ="";
-    $kutipan    ="";
-    $isi        ="";
-    $error      ="";  // ------> tampilan apabila terjadi error
-    $sukses     ="";  // ------> tampilan apabila sukses
-    
+$judul      = "";
+$kutipan    = "";
+$isi        = "";
+$error      = "";  // ------> tampilan apabila terjadi error
+$sukses     = "";  // ------> tampilan apabila sukses
+
+if (isset($_POST['simpan'])) {
+    $judul    = $_POST['judul'];
+    $kutipan  = $_POST['kutipan'];
+    $isi      = $_POST['isi'];
+
+    // jika isi judul, kutipan, tidak ada maka tampilan informasi error
+    if ($judul == '' or $isi == '') {
+        $error = "Silahkan masukkan data isi dan judul...";
+    }
+
+    //
+    if (empty($error)) {
+        $sql1   = "INSERT INTO halaman(judul,kutipan,isi) VALUES ('$judul','$kutipan','$isi')";
+        $q1     = mysqli_query($koneksi, $sql1);
+
+        if ($q1) {
+            $sukses = "SUKSES.. Data berhasil dimasukkan.";
+        } else {
+            $error  = "Gagal...input data";
+        }
+    }
+}
+
 ?>
 
 <h1>Halaman Input Admin </h1>
 <div class="mb-3 row">
     <a href="halaman.php">Kembali ke halaman Admin</a>
 </div>
+
+<?php
+if ($error) {
+?>
+    <div class="alert alert-danger" role="alert">
+        <?php echo $error ?>
+    </div>
+<?php
+}
+?>
+<?php
+if ($sukses) {
+?>
+    <div class="alert alert-primary" role="alert">
+        <?php echo $sukses ?>
+    </div>
+<?php
+}
+?>
+
 
 <form action="" method="post">
     <div class="mb-3 row">
@@ -35,7 +78,7 @@ include("inc_header.php");
     <div class="mb-3 row">
         <label for="isi" class="col-sm-2 col-form-label">Isi</label>
         <div class="col-sm-10">
-            <textarea name="isi" class="form-control" > <?php echo $isi;?> </textarea>
+            <textarea name="isi" class="form-control"> <?php echo $isi; ?> </textarea>
         </div>
     </div>
 
@@ -44,9 +87,9 @@ include("inc_header.php");
 
         </div>
         <div class="col-sm-10">
-            <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary"/>
+            <input type="submit" name="simpan" value="Simpan Data" class="btn btn-primary" />
         </div>
-</div>
+    </div>
 
 </form>
 
